b1f0f4ecf6befe6ed3bbfa3c606b83f9139516dd
Stuart Baker <stuart@smbkr.xyz>
2020-01-25 12:39:44 +0000

    Make types extensible

--- src/lib/page-parser.ts
+++ src/lib/page-parser.ts
@@ -1,12 +1,26 @@
 import cheerio from 'cheerio';
 
 class PageParser {
+  private assetTypes: AssetType[] = [
+    {
+      tag: 'img',
+      attr: 'src',
+    },
+  ];
+
   async getAssets(pageData: string): Promise<string[]> {
     const $ = cheerio.load(pageData);
-    const assets = $('img').prop('src');
+    const assets = this.assetTypes.map(assetType => {
+      return $(assetType.tag).attr(assetType.attr);
+    });
 
     return Array.isArray(assets) ? assets : [assets];
   }
+}
+
+interface AssetType {
+  tag: string;
+  attr: string;
 }
 
 export default PageParser;
