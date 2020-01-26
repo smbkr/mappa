67986c5e083ffd33b299a64b3f8f85b7904df683
Stuart Baker <stuart@smbkr.xyz>
2020-01-26 19:29:33 +0000

    Test that we don't create an infinite loop

--- src/test/unit/mapper.test.ts
+++ src/test/unit/mapper.test.ts
@@ -71,3 +71,34 @@ test('creating a map for a page that links to an internal page', async assert =>
 
   assert.deepEqual(actual, expected);
 });
+
+test('creating a map for a pair of recursive internal links', async assert => {
+  const linkGenerator = (function*() {
+    yield ['/to-you'];
+    yield ['/to-me'];
+  })();
+  const parser = ({
+    getAssets: iSpy.createSpy(() => Promise.resolve([])),
+    getLinks: iSpy.createSpy(() => {
+      return Promise.resolve(linkGenerator.next().value);
+    }),
+  } as any) as PageParser;
+  const mapper = new Mapper(parser, requesterStub);
+
+  const expected = {
+    'https://www.example.org/to-me': {
+      links: ['/to-you'],
+      assets: [],
+    },
+    'https://www.example.org/to-you': {
+      links: ['/to-me'],
+      assets: [],
+    },
+  };
+  (requesterStub.get as iSpy.Spy).reset();
+  const actual = await mapper.start('https://www.example.org/to-me');
+
+  assert.deepEqual(actual, expected);
+  const requesterCallCount = (requesterStub.get as iSpy.Spy).callCount();
+  assert.equal(2, requesterCallCount, 'only requested each page once');
+});
